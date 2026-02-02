18b67acc0096520894e53671da26a74dcc729a90
Yulia Solnechnaya <yuliasolnechnaya@gmail.com>
2026-02-02 15:38:26 +0200

    api request changed

--- src/assets/api/apiNews.js
+++ src/assets/api/apiNews.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_GNEWS_API_URL;
+const API_URL = import.meta.env.proxyUrl || import.meta.env.VITE_GNEWS_API_URL;
 const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 
 export const getNews = async () => {
