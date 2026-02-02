1be61b22c734894d08a339f2263b56670e60c98c
Yulia Solnechnaya <yuliasolnechnaya@gmail.com>
2026-02-02 17:14:44 +0200

    vercel.json+api

--- src/assets/api/apiNews.js
+++ src/assets/api/apiNews.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.GNEWS_API_URL
-const API_KEY = import.meta.env.GNEWS_API_KEY;
+const API_URL = import.meta.env.VITE_GNEWS_API_URL;
+const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 
 export const getNews = async () => {
   try {
