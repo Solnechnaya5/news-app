59fc79d549796c97bbbd95ff70fa280ada1cef8c
Yulia Solnechnaya <yuliasolnechnaya@gmail.com>
2026-02-02 16:39:35 +0200

    api

--- src/assets/api/apiNews.js
+++ src/assets/api/apiNews.js
@@ -1,33 +1,39 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const API_URL = import.meta.env.proxyUrl
-// const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
-
-// export const getNews = async () => {
-//   try {
-//     const response = await axios.get(`${API_URL}/top-headlines`, {
-//       params: {
-//         apikey: API_KEY,
-//         country: "ua",
-//         lang: "uk",
-//         max: 10,
-//       },
-//     });
-
-//     return response.data.articles ?? [];
-//   } catch (error) {
-//     console.error("GNews error:", error);
-//     return [];
-//   }
-// };
-import axios from 'axios';
+const API_URL = import.meta.env.VITE_GNEWS_API_URL
+const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 
 export const getNews = async () => {
   try {
-    const res = await axios.get('/api/news');
-    return res.data.articles ?? [];
-  } catch (e) {
-    console.error(e);
+    const response = await axios.get(`${API_URL}/top-headlines`, {
+      params: {
+        apikey: API_KEY,
+        country: "ua",
+        lang: "uk",
+        max: 10,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+
+        'Access-Control-Allow-Headers':
+
+          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+
+        'Access-Control-Allow-Methods': 'OPTIONS,POST',
+
+        'Access-Control-Allow-Credentials': true,
+
+        'Access-Control-Allow-Origin': '*',
+
+        'X-Requested-With': '*',
+
+      },
+
+    });
+
+    return response.data.articles ?? [];
+  } catch (error) {
+    console.error("GNews error:", error);
     return [];
   }
 };
