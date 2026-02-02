b5beaaa0aa556fa2ce19d77be843b7f2c7ad43ee
Yulia Solnechnaya <yuliasolnechnaya@gmail.com>
2026-02-02 16:58:29 +0200

    changed api request

--- src/assets/api/apiNews.js
+++ src/assets/api/apiNews.js
@@ -12,22 +12,7 @@ export const getNews = async () => {
         lang: "uk",
         max: 10,
       },
-      headers: {
-        'Content-Type': 'application/json',
-
-        'Access-Control-Allow-Headers':
-
-          'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-
-        'Access-Control-Allow-Methods': 'OPTIONS,POST',
-
-        'Access-Control-Allow-Credentials': true,
-
-        'Access-Control-Allow-Origin': '*',
-
-        'X-Requested-With': '*',
-
-      },
+     
 
     });
 
